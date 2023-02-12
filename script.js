d762228fffdb468d8667302e202c434eae8de7fb
dove <ndoverte11@gmail.com>
2023-02-12 18:47:06 +0800

    created error message functions

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 let form = document.getElementById('form')
 let todoInput = document.getElementById('todoInput')
 let todoList = document.querySelector('.todo__list')
+let todo__header = document.querySelector('.todo__header')
 const fragment = document.createDocumentFragment()
 
 eventListener()
 function eventListener() {
   form.addEventListener('submit', submitTodos)
+  todoList.addEventListener('click', listOptions)
 }
 
 function submitTodos(e) {
@@ -13,10 +15,24 @@ function submitTodos(e) {
   let todo = todoInput.value
 
   if (!todo) {
-    console.log('nothing')
+    uiConfirmMessage('The field is empty', 'alert alert-danger')
   } else {
     uiDisplayTodoItem(todo)
+    uiConfirmMessage('Todos added', 'alert alert-success')
+    this.reset()
   }
+}
+
+function uiConfirmMessage(msg, cls) {
+  const spanEl = document.createElement('span')
+  spanEl.textContent = msg
+  spanEl.classList = cls
+
+  todaysDate.insertAdjacentElement('afterend', spanEl)
+
+  setTimeout(() => {
+    spanEl.remove()
+  }, 1000)
 }
 
 function uiDisplayTodoItem(todo) {
@@ -30,4 +46,19 @@ function uiDisplayTodoItem(todo) {
   </div>`
   fragment.appendChild(li)
   todoList.appendChild(fragment)
+}
+
+function listOptions(e) {
+  deleteToDoIten(e.target)
+}
+
+function deleteToDoIten(item) {
+  if (item.classList.contains('fa-trash') || item.id === 'trash') {
+    const todoItemEl = item.closest('li')
+    todoItemEl.classList.remove('bounceIn')
+    todoItemEl.classList.add('bounceOutDown')
+    setTimeout(() => {
+      todoItemEl.remove()
+    }, 1000)
+  }
 }
