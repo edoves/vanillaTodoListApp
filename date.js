b583426b241438fa8c4cd88f569525b21d5dac36
dove <ndoverte11@gmail.com>
2023-02-12 12:50:40 +0800

    coded the js part for the header date

---
+++ date.js
@@ -0,0 +1,9 @@
+let todaysDay = document.getElementById('todaysDay')
+let todaysDate = document.getElementById('todaysDate')
+
+let date = new Date()
+const daysArr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+const monthsArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+
+todaysDay.textContent = daysArr[date.getDay()]
+todaysDate.textContent = `${monthsArr[date.getMonth()]} ${date.getDate()}, ${date.getFullYear()}`
